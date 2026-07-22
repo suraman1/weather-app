21682a65fa8d34da3962cf24cca11428e658a42c
suraman1 <surafelmesfin123@gmail.com>
2026-07-22 22:56:32 +0300

    Added funcitionality API request

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# weather_app
+# Weather App
+
+## An android application made to see weather data of cities.
+
+
+<P style="text-align: center;"> 
+    <img src="screenshots/app_launcher.jpg" width="250" style="margin: 10px;"/>
+    <img src="screenshots/refresh_page.jpg" width="250" style="margin: 10px;" />
+    <img src="screenshots/loading_spinner.jpg" width="250" style="margin: 10px;" />
+</p>
+
+<P style="text-align: center;"> 
+    <img src="screenshots/default_city.jpg" width="250" style="margin: 10px;"/>
+    <img src="screenshots/dropdown_cities.jpg" width="250" style="margin: 10px;"/>
+    <img src="screenshots/selected_city.jpg" width="250" style="margin: 10px;" />
+</p>
+
 
 
--- lib/widgets/additional_info.dart
+++ lib/widgets/additional_info.dart
@@ -4,7 +4,8 @@ class AdditionalInfoItem extends StatelessWidget {
   final Icon icon;
   final String label;
   final String value;
-  const AdditionalInfoItem({super.key,
+  const AdditionalInfoItem({
+    super.key,
     required this.icon,
     required this.label,
     required this.value,
@@ -14,16 +15,13 @@ class AdditionalInfoItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Icon(icon.icon,
-        size: 32,),
-        const SizedBox(height: 8,),
+        Icon(icon.icon, size: 32, color: icon.color,),
+        const SizedBox(height: 8),
         Text(label),
-        const SizedBox(height: 8,),
-        Text(value,
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
+        const SizedBox(height: 8),
+        Text(
+          value,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
       ],
     );
